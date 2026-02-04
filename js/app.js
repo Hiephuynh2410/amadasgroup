cc34b77d42fc2650b0fa7b4d0b2b33a44cfcd36b
Huỳnh Văn Hiệp <doremon8408@gmail.com>
2026-02-04 10:22:03 +0700

    fix blog6

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@
 (function () {
   "use strict";
 
-  // const __AMADAS_URL__ = new URL(window.location.href);
+  const __AMADAS_URL__ = new URL(window.location.href);
 
-  // // Detect mobile / touch device (an toàn hơn userAgent)
-  // const __AMADAS_IS_MOBILE__ =
-  //   (window.matchMedia && window.matchMedia("(pointer: coarse)").matches) ||
-  //   (window.matchMedia && window.matchMedia("(max-width: 768px)").matches);
+  // Detect mobile / touch device (an toàn hơn userAgent)
+  const __AMADAS_IS_MOBILE__ =
+    (window.matchMedia && window.matchMedia("(pointer: coarse)").matches) ||
+    (window.matchMedia && window.matchMedia("(max-width: 768px)").matches);
 
-  // //  Chỉ khóa trên desktop, mobile thì không khóa
-  // const __AMADAS_LOCK__ = !__AMADAS_IS_MOBILE__ && (__AMADAS_URL__.searchParams.get("unlock") !== "1");
-  // // State
-  // let __AMADAS_DEVTOOLS_OPEN__ = false;
-  // let __AMADAS_OVERLAY_VISIBLE__ = false;
+  //  Chỉ khóa trên desktop, mobile thì không khóa
+  const __AMADAS_LOCK__ = !__AMADAS_IS_MOBILE__ && (__AMADAS_URL__.searchParams.get("unlock") !== "1");
+  // State
+  let __AMADAS_DEVTOOLS_OPEN__ = false;
+  let __AMADAS_OVERLAY_VISIBLE__ = false;
 
   function domReady(cb) {
     if (document.readyState === "loading") {
